162e19367a8d7363de563f38aa59aca867ef2b1a
Paul Seal <prjseal@gmail.com>
2019-03-22 21:15:58 +0000

    - setting up the bare bones for the content app

---
+++ src/MediaInfo/App_Plugins/MediaInfo/js/main.controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    function MediaInfo($scope, $http) {
+
+        function init() {
+
+        }
+
+        init();
+    }
+
+    angular.module('umbraco').controller('MediaInfo', MediaInfo);
+
+})();
