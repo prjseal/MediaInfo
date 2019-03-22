4b9516394274832f86250790dfbbe07e0778a023
Paul Seal <prjseal@gmail.com>
2019-03-22 23:35:54 +0000

    - created composer and component

---
+++ src/MediaInfo/Controllers/MediaInfoBackofficeApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using Umbraco.Web.Editors;
+using Umbraco.Web.Mvc;
+using Umbraco.Core;
+using System.IO;
+
+namespace MediaInfo.Controllers
+{
+    [PluginController("MediaInfo")]
+    public class MediaInfoBackofficeApiController : UmbracoAuthorizedJsonController
+    {
+        [System.Web.Http.HttpPost]
+        public FileInfo GetFileInfo(string filePath)
+        {
+            if(File.Exists(filePath))
+            {
+                return new FileInfo(filePath);
+            }
+            return null;
+        }
+
+        public class ApiInstruction
+        {
+            public string ImageUri { get; set; }
+        }
+    }
+}
