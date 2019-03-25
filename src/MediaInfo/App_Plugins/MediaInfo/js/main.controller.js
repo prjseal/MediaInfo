35d31d7c1f695dd0c603377c910ab3e4215dce50
Paul Seal <prjseal@gmail.com>
2019-03-25 00:30:10 +0000

    - got poc working

--- src/MediaInfo/App_Plugins/MediaInfo/js/main.controller.js
+++ src/MediaInfo/App_Plugins/MediaInfo/js/main.controller.js
@@ -6,6 +6,7 @@
 
         function init() {
             apiUrl = Umbraco.Sys.ServerVariables["MediaInfo"]["MediaInfoApiUrl"];
+            $scope.data = [];
             $scope.getFileInfo();
         }
 
@@ -19,16 +20,40 @@
                 }
             }).then(function (response) {
                 console.log(response.data);
-                var text = '';
+                $scope.data = response.data;
+
+                var latDegrees = '';
+                var lngDegrees = '';
+
                 response.data.forEach(function (dir) {
-                    text += dir.Name + '\n';
-                    dir.Tags.forEach(function (tag) {
-                        text += tag.Name + ": " + tag.Description + '\n';
-                    });
-                    text += '\n';
+                    if (dir.Name === 'GPS') {
+                        dir.Tags.forEach(function (tag) {
+                            if (tag.Name === 'GPS Latitude') {
+                                latDegrees = tag.Description;
+                            } else if (tag.Name === 'GPS Longitude') {
+                                lngDegrees = tag.Description;
+                            }
+                        });
+                    }
                 });
-                $scope.text = text;
-                console.log(text);
+
+                $scope.point = new GeoPoint(lngDegrees, latDegrees);
+                var mapUrl = 'https://www.bing.com/maps/embed?h=280&w=325&cp=' + $scope.point.getLatDec() + '~' + $scope.point.getLonDec() + '&lvl=11&typ=s&sty=r&src=SHELL&FORM=MBEDV8';
+                console.log(mapUrl);
+                $scope.mapUrl = mapUrl;
+
+
+
+                //var text = '';
+                //response.data.forEach(function (dir) {
+                //    text += dir.Name + '\n';
+                //    dir.Tags.forEach(function (tag) {
+                //        text += tag.Name + ": " + tag.Description + '\n';
+                //    });
+                //    text += '\n';
+                //});
+                //$scope.data = response.data;
+                //console.log(text);
             });
         };
 
